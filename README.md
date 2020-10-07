1b3c19a8a9910522ddaff74ecd681410c66130af
Mairod <dorianlods@gmail.com>
2020-10-07 17:05:24 +0200

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+<img alt="" src="https://pbs.twimg.com/profile_images/1118495722058452993/uXevynW7_400x400.png" width="250">
+
+# Creative Dev workshop Gobelins 2021 - Dorian
+
+## Installation
+
+Clone the repository
+
+```sh
+ git clone https://github.com/mairod/Gobelins-BDDI-2021-CreativeDev-Workshop PROJECT-NAME
+ cd PROJECT-NAME
+```
+
+Install npm dependencies
+
+```sh
+ npm install 
+```
+
+To start the development server
+
+```sh
+npm start
+```
+
+To build for production
+
+```sh
+npm run build
+```
---
+++ webpack.dev.js
@@ -0,0 +1,72 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval-cheap-module-source-map',
+    entry: './src/index.js',
+    devServer: {
+        port: 3000,
+        contentBase: path.join(__dirname, "dist")
+    },
+    node: {
+        fs: 'empty'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    {
+                        // creates style nodes from JS strings
+                        loader: "style-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        // translates CSS into CommonJS
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        // compiles Sass to CSS
+                        loader: "sass-loader",
+                        options: {
+                            outputStyle: 'expanded',
+                            sourceMap: true,
+                            sourceMapContents: true
+                        }
+                    }
+                    // Please note we are not running postcss here
+                ]
+            }
+            ,
+            {
+                // Load all icons
+                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ]
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: true
+        })
+    ]
+};
