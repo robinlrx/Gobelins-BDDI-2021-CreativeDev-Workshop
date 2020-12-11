8480468b99c5002c22525453ed0e48d183f99eb4
Robin <48712314+robinlrx@users.noreply.github.com>
2020-12-11 14:20:07 +0100

    starter

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,90 +1,40 @@
-
-
 import Mouse from "./utils/mouse"
 import Easing from "./utils/easing"
 
+import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+
 const canvas = document.querySelector('.main-canvas')
-const ctx = canvas.getContext("2d")
 
-canvas.width = window.innerWidth * window.devicePixelRatio
-canvas.height = window.innerHeight * window.devicePixelRatio
-canvas.style.maxWidth = window.innerWidth
-canvas.style.maxHeight = window.innerHeight
+// Setup Scene
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer({
+   canvas: canvas
+});
 
-let canvasWidth = (canvas.width)
-let canvasHeight = (canvas.height)
-let cW2 = (canvas.width / 2)
-let cH2 = (canvas.height / 2)
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
 
-let maskLoaded = false
+window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setPixelRatio(window.devicePixelRatio)
+})
+// End Setup Scene
 
-let time = 0
+// Display axes
+const axesHelper = new THREE.AxesHelper(100);//size of axes
+scene.add( axesHelper );
 
 
 // à chaque image : 60fps
 const update = () => {
-    requestAnimationFrame(update)
+   requestAnimationFrame(update)
 
-    time += .01
 
-    let mouseX = ((Mouse.cursor[0] + 1) / 2) * canvas.width
-    let mouseY = ((Mouse.cursor[1] + 1) / 2) * canvas.height
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-    let size = 200
-
-    ctx.save()
-
-    ctx.translate(mouseX, mouseY)
-    ctx.rotate(time)
-    ctx.scale(Math.sin(time), Math.sin(time))
-
-    ctx.beginPath()
-    ctx.strokeStyle = '#ffffff'
-    ctx.moveTo(-size / 2, size / 2)
-    ctx.lineTo(0, -size / 2)
-    ctx.lineTo(size / 2, size / 2)
-    ctx.lineTo(-size / 2, size / 2)
-    ctx.stroke()
-    ctx.closePath()
-
-    ctx.restore()
-
-    // 
-
+   // Render WebGL Scene
+   renderer.render(scene, camera);
 
 }
 requestAnimationFrame(update)
-
-
-
-// let img = new Image()
-// img.src = "https://miro.medium.com/max/1068/0*WwAJP2U-pFbydOfi.jpeg"
-// img.onload = ()=>{
-//     console.log("l'image est chargée")
-// }
-
-// let mask = new Image()
-// mask.src = "https://images-ext-2.discordapp.net/external/iYBUoq3zm0M5LJDbHAX8xSMU0ZZGHQjaf60XYRcPD9U/http/designinteractif.gobelins.fr/wp-content/uploads/2018/11/cropped-Logo-Gobelins-1.png"
-// mask.onload = () => {
-//     maskLoaded = true
-// }
-
-// Blabla
-
-// let canvasWidth = (canvas.width)
-// let canvasHeight = (canvas.height)
-// let cW2 = (canvas.width / 2)
-// let cH2 = (canvas.height / 2)
-
-// ctx.beginPath()
-// ctx.strokeStyle = '#00aa00'
-// ctx.moveTo(canvasWidth / 2 - 50, canvasHeight / 2 + 50)
-// ctx.lineTo(canvasWidth / 2, canvasHeight / 2 - 50)
-// ctx.lineTo(canvasWidth / 2 + 50, canvasHeight / 2 + 50)
-// ctx.lineTo(canvasWidth / 2 - 50, canvasHeight / 2 + 50)
-// ctx.stroke()
-// ctx.closePath()
-
-
